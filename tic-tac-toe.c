cb6ae65cfc7130d75e396c5d2ce322d56a002d99
babisque <rodrigobabisque1@gmail.com>
2023-05-31 20:44:04 -0300

    Refactor game logic and UI separation

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include "tic-tac-toe.h"
+#define SYMBOL_X 'X'
+#define SYMBOL_O 'O'
 #ifdef _WIN32
     #define OS "nt"
 #elif __unix__
     #define OS "posix"
 #endif
 
-int checksequence(char *fields) {
+int checkGameOver(char *fields) {
     if (fields[0] == fields[1] && fields[1] == fields[2])
         return 1;
     else if (fields[0] == fields[4] && fields[4] == fields[8])
@@ -31,37 +34,89 @@ int checksequence(char *fields) {
         return -1;
 }
 
-void board(char x, char o, unsigned char *p1, unsigned char *p2, char *fields) {
-    int i;
-
-    if (OS == "nt") {
+void displayBoard(const char *player1, const char *player2, const char *fields) {
+    #ifdef _WIN32
         system("cls");
-    }
-    else if (OS == "posix") {
+    #elif __unix__
         system("clear");
+    #endif
+
+    printf("\n%s vs %s\n\n", player1, player2);
+
+    printf(" %c | %c | %c \n", fields[0], fields[1], fields[2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", fields[3], fields[4], fields[5]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c \n", fields[6], fields[7], fields[8]);
+    printf("\n");
+}
+
+void updateBoard(char playerSymbol, int move, char *fields, int *player) {
+            if (move == 1 && fields[0] == '1')
+                fields[0] = playerSymbol;
+            else if (move == 2 && fields[1] == '2')
+                fields[1] = playerSymbol;
+            else if (move == 3 && fields[2] == '3')
+                fields[2] = playerSymbol;
+            else if (move == 4 && fields[3] == '4')
+                fields[3] = playerSymbol;
+            else if (move == 5 && fields[4] == '5')
+                fields[4] = playerSymbol;
+            else if (move == 6 && fields[5] == '6')
+                fields[5] = playerSymbol;
+            else if (move == 7 && fields[6] == '7')
+                fields[6] = playerSymbol;
+            else if (move == 8 && fields[7] == '8')
+                fields[7] = playerSymbol;
+            else if (move == 9 && fields[8] == '9')
+                fields[8] = playerSymbol;
+            else {
+                printf("Wrong Selection\n");
+                player--;
+            }
+}
+
+void playGame(const char *player1, const char *player2) {
+    char fields[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int currentPlayer;
+    int selectedPosition;
+    char symbol;
+
+    currentPlayer = rand() % 2 + 1;
+
+    displayBoard(player1, player2, fields);
+
+    int score = -1;
+    while (score == -1) {
+        if (currentPlayer == 1)
+            printf("\n%s type the field you wanna play: ", player1);
+        else
+            printf("\n%s type the field you wanna play: ", player2);
+        scanf("%d", &selectedPosition);
+            
+        symbol = (currentPlayer == 1) ? SYMBOL_X : SYMBOL_O;
+        updateBoard(symbol, selectedPosition, fields, &currentPlayer);
+
+        score = checkGameOver(fields);
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        displayBoard(player1, player2, fields);
     }
 
-    printf("\tTic-Tac-Toe\n\n");
-    printf("\n\n");
-    printf("%s: (%c)\n%s: (%c)\n\n\n", p1, x, p2, o);
-
-    printf("    |    |    \n");
-    printf("  %c |  %c | %c\n", fields[0], fields[1], fields[2]);
-    printf("----|----|----\n");
-    printf("  %c |  %c | %c\n", fields[3], fields[4], fields[5]);
-    printf("----|----|----\n");
-    printf("  %c |  %c | %c\n", fields[6], fields[7], fields[8]);
-    printf("    |    |    \n");
+    if (score == 1) {
+        if (currentPlayer == 2) {
+            printf("\n\n%s wins!\n", player1);
+        } else {
+            printf("\n\n%s wins!\n", player2);
+        }
+    } else {
+        printf("\n\nGame draws!\n\n");
+    }
 }
 
 int main() {
     int choice;
-    char p1[20], p2[20];
-    char x, o;
-    char fields[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int player;
-    int play, score = -1;
-    char symbol;
+    char player1[20], player2[20];
+    
 
     printf("*******************************\n");
     printf("*   Welcome to Tic-Tac-Toe!   *\n");
@@ -72,64 +127,20 @@ int main() {
     printf("\nChoose what you want: ");
     scanf("%d", &choice);
 
-    switch (choice)
-    {
+    switch (choice) {
     case 1:
         do {
             printf("\nEnter name of Player 1: ");
-            scanf("%s", p1);
+            scanf("%s", player1);
             printf("Enter name of Player 2: ");
-            scanf("%s", p2);
-            !strcmp(p1, p2) ? printf("Enter different names for players\n\n") : 0;
-        } while (!strcmp(p1, p2));
-        x = 'X';
-        o = '0';
+            scanf("%s", player2);
+            if (strcmp(player1, player2) == 0) {
+                printf("Enter different names for players\n\n");
+            }
+        } while (strcmp(player1, player2) == 0);
         
-        board(x, o, p1, p2, fields);
-
-        do {
-            player = ((player % 2) ? 1 : 2);
-            if (player == 1)
-                printf("\n%s type the field you wanna play: ", p1);
-            else
-                printf("\n%s type the field you wanna play: ", p2);
-            scanf("%d", &play);
-            symbol = ((player == 1) ? x : o);
-            if (play == 1 && fields[0] == '1')
-                fields[0] = symbol;
-            else if (play == 2 && fields[1] == '2')
-                fields[1] = symbol;
-            else if (play == 3 && fields[2] == '3')
-                fields[2] = symbol;
-            else if (play == 4 && fields[3] == '4')
-                fields[3] = symbol;
-            else if (play == 5 && fields[4] == '5')
-                fields[4] = symbol;
-            else if (play == 6 && fields[5] == '6')
-                fields[5] = symbol;
-            else if (play == 7 && fields[6] == '7')
-                fields[6] = symbol;
-            else if (play == 8 && fields[7] == '8')
-                fields[7] = symbol;
-            else if (play == 9 && fields[8] == '9')
-                fields[8] = symbol;
-            else {
-                printf("Wrong Selection\n");
-                player--;
-            }
-            score = checksequence(fields);
-            player++;
-            board(x, o, p1, p2, fields);
-        }while (score == -1);
-        if (score == 1) {
-            if (player == 2) {
-                printf("\n\n%s wins!\n", p1);
-            } else {
-                printf("\n\n%s wins!\n", p2);
-            }
-        } else {
-            printf("\n\nGame draws!\n\n");
-        }
+        srand(time(NULL));
+        playGame(player1, player2);        
         break;
     
     default:
--- tic-tac-toe.h
+++ tic-tac-toe.h
@@ -1,2 +1,4 @@
 int checksequence(char *fields);
-void board(char x, char o, unsigned char *p1, unsigned char *p2, char *fields);
+void displayBoard(const char *player1, const char *player2, const char *fields);
+void updateboard(char symbol, int play, char *fields, int *player);
+void playGame(const char *player1, const char *player2);
